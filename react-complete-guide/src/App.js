1b3e5876fef7ee796ebadf234eacdbcabf5d60dd
Logan Wood <lwood3499@gmail.com>
2018-12-16 11:46:49 -0600

    changing state and props

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -3,13 +3,50 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Logan', age: 36 },
+      { name: 'Tiffany', age: 36 },
+      { name: 'Amrynn', age: 7 },
+      { name: 'Eisley', age: 7 }
+    ]
+  };
+
+  switchNameHandler = () => {
+    // console.log('switchNameHandler was clicked');
+    this.setState({
+      persons: [
+        { name: 'Logan James', age: 36 },
+        { name: 'Tiffany Annette', age: 36 },
+        { name: 'Amrynn Elise', age: 7 },
+        { name: 'Eisley Quinn', age: 7 }
+      ]
+    });
+  };
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App!</h1>
-        <Person />
-        <Person />
-        <Person />
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        >
+          My hobbies: Programming
+        </Person>
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+        />
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        />
+        <Person
+          name={this.state.persons[3].name}
+          age={this.state.persons[3].age}
+        />
       </div>
     );
   }
--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
-const person = () => {
-  return <p>I'm a Person! I'm {Math.floor(Math.random() * 30)} years old!</p>;
+const person = props => {
+  return (
+    <div>
+      <p>
+        I'm a {props.name}! I'm {props.age} years old!
+      </p>
+      <p>{props.children}</p>
+    </div>
+  );
 };
 
 export default person;
