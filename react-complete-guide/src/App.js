a423f3731065f8f74ef003344c908042037c988f
Logan Wood <lwood3499@gmail.com>
2018-12-18 19:36:21 -0600

    rendering lists dynamically

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -57,26 +57,9 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-          <Person
-            name={this.state.persons[0].name}
-            age={this.state.persons[0].age}
-            changed={this.nameChangedHandler}
-          >
-            My hobbies: Programming
-          </Person>
-          <Person
-            name={this.state.persons[1].name}
-            age={this.state.persons[1].age}
-          />
-          <Person
-            name={this.state.persons[2].name}
-            age={this.state.persons[2].age}
-            click={this.switchNameHandler.bind(this, 'Log!')}
-          />
-          <Person
-            name={this.state.persons[3].name}
-            age={this.state.persons[3].age}
-          />
+          {this.state.persons.map(person => {
+            return <Person name={person.name} age={person.age} />;
+          })}
         </div>
       );
     }
