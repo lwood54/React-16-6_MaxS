a54bec2f30d771e7ed429e2bd92900b3da139f58
Logan Wood <lwood3499@gmail.com>
2018-12-21 08:54:38 -0600

    added inline styling random char colors added

--- react-complete-guide/src/CharComponent/CharComponent.js
+++ react-complete-guide/src/CharComponent/CharComponent.js
@@ -1,14 +1,20 @@
 import React from 'react';
 
 const charComponent = props => {
+  let randomNum1 = Math.floor(Math.random() * 255 + 1);
+  let randomNum2 = Math.floor(Math.random() * 255 + 1);
+  let randomNum3 = Math.floor(Math.random() * 255 + 1);
+
   let inlineStyle = {
     display: 'inline-block',
     padding: '16px',
     textAlign: 'center',
     margin: '16px',
     border: '1px solid black',
-    cursor: 'pointer'
+    cursor: 'pointer',
+    backgroundColor: `rgb(${randomNum1}, ${randomNum2}, ${randomNum3})`
   };
+
   return (
     <p style={inlineStyle} onClick={props.click}>
       {props.charProp}
