1b568ed2ed5017cf29f1f0739732a8ff8bcd5c31
Logan Wood <lwood3499@gmail.com>
2018-12-29 20:37:46 -0700

    passing unkown props to higher order components

--- react-complete-guide/src/hoc/WithClass.js
+++ react-complete-guide/src/hoc/WithClass.js
@@ -1,11 +1,28 @@
-import React from 'react';
+// import React from 'react';
+import React, { Component } from 'react';
 
+//////////////////  you can return a stateless funcitonal component as the wrapper component /////////////////////////
+// const withClass = (WrappedComponent, className) => {
+//   return props => (
+//     <div className={className}>
+//       <WrappedComponent {...props} />{' '}
+//       {/* you can just pass on any props that are found in the component object*/}
+//     </div>
+//   );
+// };
+
+//////////////////  you can also return a stateful class based component as the wrapper component /////////////////////////
 const withClass = (WrappedComponent, className) => {
-  return props => (
-    <div className={className}>
-      <WrappedComponent />
-    </div>
-  );
+  return class extends Component {
+    render() {
+      return (
+        <div className={className}>
+          <WrappedComponent {...this.props} />{' '}
+          {/* you can just pass on any props that are found in the component object*/}
+        </div>
+      );
+    }
+  };
 };
 
 export default withClass;
