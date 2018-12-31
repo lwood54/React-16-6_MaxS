1ba92cad070c39f41a2cd3aca8ef831af2786b49
Logan Wood <lwood3499@gmail.com>
2018-12-31 07:55:51 -0700

    adding a ref for focus on input in Person.js

--- react-complete-guide/src/components/Persons/Persons.js
+++ react-complete-guide/src/components/Persons/Persons.js
@@ -55,6 +55,7 @@ class Persons extends PureComponent {
       return (
         <Person
           click={() => this.props.clicked(index)}
+          position={index}
           name={person.name}
           age={person.age}
           key={person.id}
